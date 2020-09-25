3be3593408e3aeb1d5e393df605165a678e02788
atrinidad3 <atrinidad3@dxc.com>
2020-09-25 01:41:03 -0400

    Add labels to chart

--- Code/lee.py
+++ Code/lee.py
@@ -7,6 +7,18 @@ import ast
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route('/')
 def home():
--- Code/static/js/charts.js
+++ Code/static/js/charts.js
@@ -9,8 +9,8 @@ function loadChart() {
             labels: ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"],
             datasets: [{
                 // label: "Population (millions)",
-                // backgroundColor: ["#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f", "#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f"],
-                backgroundColor: ["#17406D", "#074986", "#095397", "#0E6BC0", "#487ECA", "#7A99D2", "#98ADDA", "#B2C0E1", "#C7D1E9", "#DCF7BA"],
+                backgroundColor: ["#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f", "#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f"],
+                // backgroundColor: ["#17406D", "#074986", "#095397", "#0E6BC0", "#487ECA", "#7A99D2", "#98ADDA", "#B2C0E1", "#C7D1E9", "#DCF7BA"],
                 data: [2478, 5267, 734, 784, 433, 2478, 5267, 734, 784, 433]
             }]
         },
@@ -26,10 +26,14 @@ function loadChart() {
                 text: 'Predicted world population (millions) in 2050'
             },
             plugins: {
-                labels: {
-                    render: 'label'
-
-                }
+                labels: [{
+                        render: 'label',
+                        position: 'outside'
+                    },
+                    {
+                        render: 'percentage'
+                    }
+                ]
             }
         }
     });

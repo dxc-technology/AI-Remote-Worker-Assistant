7fe654be6cd392927ae6c1a4a5d2d07c96a24ca1
Om Prakash Singh <55798799+ompsingh@users.noreply.github.com>
2020-09-25 17:09:53 -0400

    Update lee.py

--- lee.py
+++ lee.py
@@ -7,7 +7,7 @@ import pulp
 from pulp import LpVariable,LpProblem,LpStatus,LpMaximize,LpMinimize
     
 
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -57,7 +57,7 @@ def optimize():
     mmonthly_health = 200.0
     mmonthly_maintenance = 110.0
     # Actual expense for each category
-    ##_input = request.args['input']
+    _input = request.args['input']
     monthly_home_utility_spend = 460.0
     monthly_transportation_spend = 110.0
     monthly_shopping_groceries_spend = 100.0
@@ -130,19 +130,19 @@ def optimize():
     minimized_travel = monthly_travel_spend * ( 1 - pulp.value(travel_perct_adjustment))
     minimized_health = monthly_health_spend * ( 1 - pulp.value(health_perct_adjustment))
     minimized_maintenance = monthly_maintenance_spend  * ( 1 - pulp.value(maintenance_perct_adjustment))
-    return make_response(jsonify({
-        "minimized_home_utility": minimized_home_utility,
-        "minimized_transportation": minimized_transportation,
-        "minimized_shopping_groceries": minimized_shopping_groceries,
-        "minimized_personal_family_care": minimized_personal_family_care,
-        "minimized_restaurant_dinning": minimized_restaurant_dinning,
-        "minimized_insurance": minimized_insurance,
-        "minimized_entertainment": minimized_entertainment,
-        "minimized_travel": minimized_travel,
-        "minimized_health": minimized_health,
-        "minimized_maintenance": minimized_maintenance,
-    }),200)
-    #return _input
+#     return make_response(jsonify({
+#         "minimized_home_utility": minimized_home_utility,
+#         "minimized_transportation": minimized_transportation,
+#         "minimized_shopping_groceries": minimized_shopping_groceries,
+#         "minimized_personal_family_care": minimized_personal_family_care,
+#         "minimized_restaurant_dinning": minimized_restaurant_dinning,
+#         "minimized_insurance": minimized_insurance,
+#         "minimized_entertainment": minimized_entertainment,
+#         "minimized_travel": minimized_travel,
+#         "minimized_health": minimized_health,
+#         "minimized_maintenance": minimized_maintenance,
+#     }),200)
+    return _input
 if __name__ == '__main__':
     port = int(os.getenv('PORT'))
     print("Starting app on port %d" % port)

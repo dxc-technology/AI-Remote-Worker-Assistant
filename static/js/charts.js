4c9e5dbc29d532efb8799f9002626a4209f940f3
atrinidad3 <atrinidad3@dxc.com>
2020-09-26 17:35:10 -0400

    Working chart fetch - 3 secs

--- static/js/charts.js
+++ static/js/charts.js
@@ -40,90 +40,90 @@
 
 // 'home', 'transportation', 'shopping', 'personal', 'restaurant', 'insurance', 'entertainment', 'travel', 'health', 'maintenance', 'savings'
 
-function loadChart(url) {
+// function loadChart(url) {
 
-    // const myApi = url;
-    const myApi = "https://cors-anywhere.herokuapp.com/https://hello-lee.herokuapp.com/optimize";
-    var details = "";
-    var details1 = "";
-    var details2 = "";
-    var details3 = "";
-    var details4 = "";
-    var details5 = "";
-    var details6 = "";
-    var details7 = "";
-    var details8 = "";
-    var details9 = "";
+//     // const myApi = url;
+//     const myApi = "https://cors-anywhere.herokuapp.com/https://hello-lee.herokuapp.com/optimize";
+//     var details = "";
+//     var details1 = "";
+//     var details2 = "";
+//     var details3 = "";
+//     var details4 = "";
+//     var details5 = "";
+//     var details6 = "";
+//     var details7 = "";
+//     var details8 = "";
+//     var details9 = "";
 
-    async function getAPI(url) {
-        const res = await fetch(url);
-        const mydata = await res.json();
-        details = mydata.home;
-        details1 = mydata.transportation;
-        details2 = mydata.shopping;
-        details3 = mydata.personal;
-        details4 = mydata.restaurant;
-        details5 = mydata.insurance;
-        details6 = mydata.entertainment;
-        details7 = mydata.travel;
-        details8 = mydata.health;
-        details9 = mydata.maintenance;
-        console.log(details)
-        console.log(mydata);
-
-
-        //Doughnut Chart
-        new Chart(document.getElementById("chartBudget"), {
-            type: 'doughnut',
-            data: {
-                labels: ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"],
-                datasets: [{
-                    // label: "Population (millions)",
-                    // backgroundColor: ["#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f", "#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f"],
-                    // backgroundColor: ["#17406D", "#074986", "#095397", "#0E6BC0", "#487ECA", "#7A99D2", "#98ADDA", "#B2C0E1", "#C7D1E9", "#DCF7BA"],
-                    backgroundColor: ["#6F2C91", "#40BADA", "#DF6D27", "#DDB726", "#0067B3", "#8FB73E", "#6F2C91", "#40BADA", "#DF6D27", "#DDB726", "#0067B3", ],
-                    data: [details, details1, details2, details3, details4, details5, details6, details7, details8, details9]
-                }]
-            },
-            options: {
-                legend: {
-                    display: false,
-                    labels: {
-                        fontColor: '#5c6dc0',
-                    }
-                },
-                title: {
-                    display: false,
-                    text: 'Predicted world population (millions) in 2050'
-                },
-                plugins: {
-                    labels: [{
-                            render: 'image',
-                            images: [
-                                { src: '/static/icons/Home.png', width: 32, height: 22 },
-                                { src: '/static/icons/Home.png', width: 32, height: 22 },
-                                { src: '/static/icons/Home.png', width: 32, height: 22 }
-                            ]
-                        },
-                        {
-                            render: 'value',
-                            position: 'border',
-                            fontColor: 'white',
-                            textMargin: 4,
-                            outsidePadding: 4,
-                            fontSize: 12,
-                            textShadow: true,
-                            fontStyle: 'bold'
-                        }
-                    ]
-                }
-            }
-        });
-
-    }
-    console.log(details)
-    getAPI(myApi);
+//     async function getAPI(url) {
+//         const res = await fetch(url);
+//         const mydata = await res.json();
+//         details = mydata.home;
+//         details1 = mydata.transportation;
+//         details2 = mydata.shopping;
+//         details3 = mydata.personal;
+//         details4 = mydata.restaurant;
+//         details5 = mydata.insurance;
+//         details6 = mydata.entertainment;
+//         details7 = mydata.travel;
+//         details8 = mydata.health;
+//         details9 = mydata.maintenance;
+//         console.log(details)
+//         console.log(mydata);
 
 
-}
-// })(jQuery);
+//         //Doughnut Chart
+//         new Chart(document.getElementById("chartBudget"), {
+//             type: 'doughnut',
+//             data: {
+//                 labels: ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"],
+//                 datasets: [{
+//                     // label: "Population (millions)",
+//                     // backgroundColor: ["#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f", "#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f"],
+//                     // backgroundColor: ["#17406D", "#074986", "#095397", "#0E6BC0", "#487ECA", "#7A99D2", "#98ADDA", "#B2C0E1", "#C7D1E9", "#DCF7BA"],
+//                     backgroundColor: ["#6F2C91", "#40BADA", "#DF6D27", "#DDB726", "#0067B3", "#8FB73E", "#6F2C91", "#40BADA", "#DF6D27", "#DDB726", "#0067B3", ],
+//                     data: [details, details1, details2, details3, details4, details5, details6, details7, details8, details9]
+//                 }]
+//             },
+//             options: {
+//                 legend: {
+//                     display: false,
+//                     labels: {
+//                         fontColor: '#5c6dc0',
+//                     }
+//                 },
+//                 title: {
+//                     display: false,
+//                     text: 'Predicted world population (millions) in 2050'
+//                 },
+//                 plugins: {
+//                     labels: [{
+//                             render: 'image',
+//                             images: [
+//                                 { src: '/static/icons/Home.png', width: 32, height: 22 },
+//                                 { src: '/static/icons/Home.png', width: 32, height: 22 },
+//                                 { src: '/static/icons/Home.png', width: 32, height: 22 }
+//                             ]
+//                         },
+//                         {
+//                             render: 'value',
+//                             position: 'border',
+//                             fontColor: 'white',
+//                             textMargin: 4,
+//                             outsidePadding: 4,
+//                             fontSize: 12,
+//                             textShadow: true,
+//                             fontStyle: 'bold'
+//                         }
+//                     ]
+//                 }
+//             }
+//         });
+
+//     }
+//     console.log(details)
+//     getAPI(myApi);
+
+
+// }
+// // })(jQuery);

dfbcbb71100d66fc5838846a27d200762611f321
atrinidad3 <atrinidad3@dxc.com>
2020-09-25 21:08:34 -0400

    Chart

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,7 +1,20 @@
 // (function($) {
 //     'use strict';
 
+
+
+
+
+
 function loadChart() {
+
+    fetch("https://hello-lee.herokuapp.com/optimize")
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data:", data)
+        });
+
+
     //Doughnut Chart
     new Chart(document.getElementById("chartBudget"), {
         type: 'doughnut',
@@ -11,9 +24,7 @@ function loadChart() {
                 // label: "Population (millions)",
                 // backgroundColor: ["#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f", "#357ffa", "#f7b11b", "#ff6c60", "#8663e1", "#08bf6f"],
                 // backgroundColor: ["#17406D", "#074986", "#095397", "#0E6BC0", "#487ECA", "#7A99D2", "#98ADDA", "#B2C0E1", "#C7D1E9", "#DCF7BA"],
-                backgroundColor: ["#6F2C91", "#40BADA", "#DF6D27", "#DDB726", "#0067B3", "#8FB73E", "#6F2C91", "#40BADA", "#DF6D27", "#DDB726", "#0067B3",
-
-                ],
+                backgroundColor: ["#6F2C91", "#40BADA", "#DF6D27", "#DDB726", "#0067B3", "#8FB73E", "#6F2C91", "#40BADA", "#DF6D27", "#DDB726", "#0067B3", ],
                 data: [2478, 5267, 734, 784, 433, 2478, 5267, 734, 784, 433]
             }]
         },
